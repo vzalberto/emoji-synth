3b9dfa21278fc28257695537071a25b362cc350e
Alberto Zavala <alberto@getclue.com>
2020-12-23 14:57:15 -0600

    state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import * as Tone from 'tone'
+import React, {useState} from 'react'
 
 const App = () => {
   const synth = new Tone.Synth().toDestination();
-  let octave = 4;
 
   const diatonic = ["C", "D", "E", "F", "G", "A", "B"]
   const dissonance = ["C", "C#", "F", "F#", "G", "A", "B"]
 
-  let scale = diatonic;
+  const [octave, setOctave] = useState(4);
+  const [scale, setScale] = useState(diatonic)
+
 
   const play = (note) => {
     synth.triggerAttackRelease(note, "8n")
@@ -37,13 +39,15 @@ const App = () => {
         </div>
         <div>
           <span>octava</span>
-          <button onClick={()=>octave = octave - 1}>👎</button>
-          <button onClick={()=>octave = octave + 1}>👍</button>
+          <button onClick={()=>setOctave(octave - 1)}>👎</button>
+          <button onClick={()=>setOctave(octave + 1)}>👍</button>          <span></span>
+          <span>{octave}</span>
         </div>
         <div>
           <span>escala</span>
-          <button onClick={()=>scale = diatonic}>🤲</button>
-          <button onClick={()=>scale = dissonance}>🥴</button>
+          <button onClick={()=>setScale(diatonic)}>🤲</button>
+          <button onClick={()=>setScale(dissonance)}>🥴</button>
+          <span>{scale}</span>
         </div>
       </header>
     </div>
